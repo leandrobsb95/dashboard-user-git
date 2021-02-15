2f42c7437b8a5a5232bedc65a68c76ac90521f83
leandrobsb95 <leandrobsb95@gmail.com>
2021-02-15 11:45:37 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dug-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dashboard-user-git';
+}
